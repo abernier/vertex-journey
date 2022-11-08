b208f329752a5dee72e086a9f70383f32b266531
Antoine BERNIER <antoine.bernier@gmail.com>
2022-11-08 17:00:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+[CodeSandbox](https://codesandbox.io/s/github/abernier/tpl-three)
+
+# INSTALL
+
+Pre-requisites:
+
+- Node
+
+```sh
+$ npm ci
+```
+
+# Dev
+
+```sh
+$ npm start
+```
+
+# Build
+
+```sh
+$ npm run build
+```
+
+A Github Actions [deploy](.github/workflows/deploy.yml) task will build and deploy to `gh-pages` branch when pushing on `main`. Resulting app will be available at: https://{username}.github.io/{reponame}
+
+NB: Make sure you have Github Pages enabled in your [project's Settings](/../../settings/pages)
+<img width="748" alt="197813061-d94a75e6-8525-402f-8786-ec9f0bb04b13" src="https://user-images.githubusercontent.com/76580/197829753-9ac58012-f487-4456-b810-e8798732ec24.png">
---
+++ src/index.js
@@ -0,0 +1,187 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min";
+
+import "./index.css";
+
+const conf = {
+  bg: "#484848",
+  fov: 50,
+  grid: true,
+  axes: true,
+};
+window.conf = conf;
+
+//
+// scene
+//
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(conf.bg);
+
+THREE.ColorManagement.legacyMode = false;
+
+//
+// 🎥 camera
+//
+
+const camera = new THREE.PerspectiveCamera(
+  conf.fov,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.set(7, 4, 21);
+
+//
+// 📷 renderer
+//
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping; // https://threejs.org/docs/#api/en/constants/Renderer
+// renderer.toneMappingExposure = 2.3;
+// renderer.physicallyCorrectLights = true;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // https://threejs.org/docs/#api/en/constants/Renderer
+document.body.appendChild(renderer.domElement);
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//
+// Meshes
+//
+
+// 🧊 cube
+
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshStandardMaterial({ color: "blue" })
+);
+cube.castShadow = true;
+cube.position.y = 1;
+scene.add(cube);
+
+// 🏀 sphere
+
+const sphere = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(1, 1),
+  new THREE.MeshStandardMaterial({ color: "red", flatShading: true })
+);
+sphere.castShadow = true;
+sphere.position.y = 1;
+sphere.position.z = 5;
+scene.add(sphere);
+
+// 🛬 ground plane
+
+const ground = new THREE.Mesh(
+  new THREE.BoxGeometry(100, 100, 0.1),
+  new THREE.MeshStandardMaterial({
+    color: "gray",
+    transparent: true,
+    opacity: 0.8,
+  })
+);
+ground.position.y = -0.1 / 2;
+ground.receiveShadow = true;
+ground.rotation.x = -Math.PI / 2;
+scene.add(ground);
+
+//
+// 💡 lights
+//
+
+// 🔦 spot
+
+const spotLight = new THREE.SpotLight("white");
+spotLight.position.set(15, 15, 15);
+spotLight.penumbra = 1;
+spotLight.castShadow = true;
+spotLight.intensity = 2;
+spotLight.shadow.bias = -0.0001;
+// spotLight.shadow.mapSize.width = 1024 * 4;
+// spotLight.shadow.mapSize.height = 1024 * 4;
+// spotLight.shadow.camera.near = 0.5; // default 0.5
+// spotLight.shadow.camera.far = 10; // default 500
+
+scene.add(spotLight);
+// scene.add(new THREE.SpotLightHelper(spotLight));
+
+// 🌤️ ambient
+
+const ambientLight = new THREE.AmbientLight();
+ambientLight.intensity = 0.2;
+scene.add(ambientLight);
+
+//
+// 📐 dummies
+//
+
+const gridHelper = new THREE.GridHelper(30, 30);
+scene.add(gridHelper);
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+//
+// 🎛️ GUI
+//
+
+const gui = new GUI(); // see: https://lil-gui.georgealways.com/
+
+gui
+  .add(conf, "bg")
+  .name("fov")
+  .onChange((val) => (scene.background = new THREE.Color(val)));
+
+gui
+  .addFolder("camera")
+  .close()
+  .add(conf, "fov")
+  .name("fov")
+  .onChange((val) => {
+    camera.fov = val;
+    camera.updateProjectionMatrix();
+  });
+
+gui
+  .add(conf, "grid")
+  .name("grid")
+  .onChange((val) => {
+    gridHelper.visible = val;
+  });
+
+gui
+  .add(conf, "axes")
+  .name("axes")
+  .onChange((val) => {
+    axesHelper.visible = val;
+  });
+
+//
+// 🎬 animation
+//
+
+function animate(t) {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+  stats.update();
+}
+animate();
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", onWindowResize, false);
